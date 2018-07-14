c27499f22decc4c6e0e99dd5a3c750f19de453dc
serg <prog@mailoffice.info>
2018-07-14 17:52:03 +0300

    use middleware

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import reducer from './reducers';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+        reducer, 
+        composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
     <Provider store={store}>
